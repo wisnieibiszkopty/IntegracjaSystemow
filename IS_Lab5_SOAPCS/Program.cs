a89e658dede0d7a55412537a39edff38b4569602
student <s97766@pollub.edu.pl>
2024-03-22 13:03:45 +0100

    Ukończono lab5

---
+++ IS_Lab5_SOAPCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using ServiceReference1;
+
+class Client
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("My First SOAP Client!");
+        MyFirstSOAPInterfaceClient client = new MyFirstSOAPInterfaceClient();
+
+        string text = await client.getHelloWorldAsStringAsync("Kamil");
+        Console.WriteLine(text);
+
+        long days = await client.getDaysBetweenDatesAsync("22 03 2024", "26 03 2024");
+        Console.WriteLine(days);
+    }
+}
+
